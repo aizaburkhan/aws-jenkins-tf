cb45170c51c4be919e41c927a50f7dc484b66660
aizaburkhan <aizada.wedf@gmail.com>
2024-01-10 18:12:21 +0000

    Added bucket, dynamodb table in # and tfvars. When apply terraform we need to specify the tfvars file pre US region

--- vpc-group-4/backend.tf
+++ vpc-group-4/backend.tf
@@ -6,3 +6,26 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "s3-statefile-backends"
+  # region = var.region
+}
+
+# resource "aws_dynamodb_table" "basic-dynamodb-table" {
+#   name           = "lock-state"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = 5
+#   write_capacity = 5
+#   hash_key       = "TestTableHashKey"
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+
+#   tags = {
+#     Name        = "lock-state"
+#     Environment = "backends-by-region"
+#   }
+# }
