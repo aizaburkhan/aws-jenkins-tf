09ee8a2c06bacbfce2c6f1bb18ca0fe4a94852cb
akadyrakunov <akylbek.kadyrakun@gmail.com>
2024-01-13 18:43:38 +0000

    final project add ups

--- vpc-group-4/backend.tf
+++ vpc-group-4/backend.tf
@@ -1,8 +1,9 @@
 # virginia/terraform.tfstate
 terraform {
   backend "s3" {
-    bucket = "3-statefile-backends-ak"
+    bucket = "3-statefile-backends-ak" # << Replace the S3 Bucket name
     key    = "statefile/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "group-4-dynamoDB" # << Replace the DynamoDB Table name
   }
 }
